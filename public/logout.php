725abfb1c3bcced591ce21de249b525f95edbee5
FelipeZuquettiPereira <felipe_z_pereira@estudante.sesisenai.org.br>
2026-06-01 12:53:15 -0300

    doc - logout documentado

--- public/logout.php
+++ public/logout.php
@@ -1,8 +1,10 @@
 <?php
 
-    session_start();
-    session_destroy();
-    header("Location: ../index.php");
-    exit();
+    session_start(); // Inicia a sessão para acessar as variáveis de sessão
+    session_destroy(); // Destroi a sessão, removendo todas as variáveis de sessão e encerrando a sessão atual
+    header("Location: ../index.php"); // Redireciona o usuário para a página de login (index.php) após o logout
+    exit(); // Encerra a execução do script para garantir que o redirecionamento ocorra imediatamente
 
 ?>
+
+// O código acima é responsável por encerrar a sessão do usuário, efetivamente fazendo o logout. Ele inicia a sessão, destrói todas as variáveis de sessão e redireciona o usuário para a página de login. É uma prática comum em sistemas de autenticação para garantir que o usuário seja desconectado de forma segura.
